7cb2370d254960c22441099556668c96cc5a5139
SaurabhG02 <59133760+SaurabhG02@users.noreply.github.com>
2021-05-07 23:36:55 +0530

    postman testing

--- APIProj/api/serializers.py
+++ APIProj/api/serializers.py
@@ -3,16 +3,8 @@ from .models import Article
 
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
-        model : Article
+        model = Article
         fields = ['id', 'title', 'description']
 
 
-    # title = serializers.CharField(max_length=100)
-    # description = serializers.CharField(ax_length=400)
-
-    # def create(self, validated_data):
-    #     return Article.objects.create(validated_data)
-
-    # def update(self, instance, validated_data):
-    #     instance.title = validated_data.get('title', instance.title)
-    #     instance.description = validated_data.get('description', instance.description)
+ 
